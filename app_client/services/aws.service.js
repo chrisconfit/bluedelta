af8d70b94df38d1aa5d4d56b0a045a45949f9c3d
Jake Corn <jakecorn@Jakes-MacBook-Pro.local>
2017-05-18 16:15:53 -0500

    finishing adding methods to aws service

--- app_client/services/aws.service.js
+++ app_client/services/aws.service.js
@@ -72,16 +72,33 @@
     }
 
     authenticateCognitoUser = function(username, password) {
-      var authenticationData = {
-        Username : username,
-        Password : password,
-      };
-      var authenticationDetails = new AWSCognito.CognitoIdentityServiceProvider.AuthenticationDetails(authenticationData);
-      var poolData = { UserPoolId : 'us-east-1_TcoKGbf7n', ClientId : '4pe2usejqcdmhi0a25jp4b5sh3' };
-      var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
-      var userData = { Username : username, Pool : userPool };
-      var cognitoUser = new AWSCognito.CognitoIdentityServiceProvider.CognitoUser(userData);
+      
+      function getAuthenticationDetails(uname, pword) {
+        var authenticationData = {
+          Username : uname,
+          Password : pword,
+        };
+        var authenticationDetails = new AWSCognito.CognitoIdentityServiceProvider.AuthenticationDetails(authenticationData);
+        return authenticationDetails;
+      }
 
+      function getUserPool() {
+        var poolData = { UserPoolId : 'us-east-1_TcoKGbf7n', ClientId : '4pe2usejqcdmhi0a25jp4b5sh3' };
+        var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
+        return userPool;
+      }
+
+      function getCognitoUser(uname, upool) {
+        var userData = { Username : uname, Pool : upool };
+        var cognitoUser = new AWSCognito.CognitoIdentityServiceProvider.CognitoUser(userData);
+        return cognitoUser;
+      }
+    
+      var authenticationDetails = getAuthenticationDetails(username, password)
+      var userPool    = getUserPool();
+      var cognitoUser = getCognitoUser(username, userPool);
+      
+      
       cognitoUser.authenticateUser(authenticationDetails, {
           onSuccess: function (result) {
               console.log('access token + ' + result.getAccessToken().getJwtToken());
@@ -113,10 +130,16 @@
     }
 
     updateAttributesOnUser = function(username, obj) {
-
-      // JAKE! add stuff to make this function referentially transparent
-
-
+      function getUserPool() {
+        var poolData = { UserPoolId : 'us-east-1_TcoKGbf7n', ClientId : '4pe2usejqcdmhi0a25jp4b5sh3' };
+        var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
+        return userPool;
+      }
+      function getCognitoUser(uname, upool) {
+        var userData = { Username : uname, Pool : upool };
+        var cognitoUser = new AWSCognito.CognitoIdentityServiceProvider.CognitoUser(userData);
+        return cognitoUser;
+      }
       function buildAttribute(keyName, value) {
         var attribute = {
           Name: keyName,
@@ -125,7 +148,6 @@
         var result = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserAttribute(attribute);
         return result;
       }
-
       function buildAttributeList(attributeBuilder, obj) {
         var attrList = [];
         var attrToAdd;
@@ -135,11 +157,170 @@
         }
         return attrList;
       }
-
-
-
+      var attributeList = buildAttributeList(buildAttribute, obj);
+      var cognitoUser   = getCognitoUser(username, getUserPool());
+      cognitoUser.updateAttributes(attributeList, function(err, result) {
+          if (err) {
+              alert(err);
+              return;
+          }
+          console.log('call result: ' + result);
+      });
     }
 
+    deleteUserAttributes = function(cognitoUser, attributeList) {
+      cognitoUser.deleteAttributes(attributeList, function(err, result) {
+          if (err) {
+              alert(err);
+              return;
+          }
+          console.log('call result: ' + result);
+      });
+    }
+
+    verifyUserAttribute = function(cognitoUser, attributeName) {
+        cognitoUser.getAttributeVerificationCode(attributeName, {
+          onSuccess: function (result) {
+              console.log('call result: ' + result);
+          },
+          onFailure: function(err) {
+              alert(err);
+          },
+          inputVerificationCode: function() {
+              var verificationCode = prompt('Please input verification code: ' ,'');
+              cognitoUser.verifyAttribute(attributeName, verificationCode, this);
+          }
+      });
+    }
+
+    retrieveUserAttributes = function(cognitoUser) {
+      cognitoUser.getUserAttributes(function(err, result) {
+          if (err) {
+              alert(err);
+              return;
+          }
+          for (i = 0; i < result.length; i++) {
+              console.log('attribute ' + result[i].getName() + ' has value ' + result[i].getValue());
+          }
+      });
+    }
+
+    resendConfirmationCode = function(cognitoUser) {
+      cognitoUser.resendConfirmationCode(function(err, result) {
+          if (err) {
+              alert(err);
+              return;
+              }
+              alert(result);
+      });
+    }
+
+    confirmRegistration = function(cognitoUser, confirmationCode) {
+      cognitoUser.confirmRegistration(confirmationCode, true, function(err, result) {
+          if (err) {
+              alert(err);
+              return;
+          }
+          alert(result);
+      });
+    }
+
+    changePassword = function(cognitoUser, oldPassword, newPassword) {
+      cognitoUser.changePassword(oldPassword, newPassword, function(err, result) {
+        if (err) {
+            alert(err);
+            return;
+        }
+        console.log('call result: ' + result);
+      });
+    }
+
+    forgotPasswordFlow = function(cognitoUser) {
+      cognitoUser.forgotPassword({
+        onSuccess: function (result) {
+            console.log('call result: ' + result);
+        },
+        onFailure: function(err) {
+            alert(err);
+        },
+        inputVerificationCode() {
+            var verificationCode = prompt('Please input verification code ' ,'');
+            var newPassword = prompt('Enter new password ' ,'');
+            cognitoUser.confirmPassword(verificationCode, newPassword, this);
+        }
+      });
+    }
+
+    deleteUser = function(cognitoUser) {
+      cognitoUser.deleteUser(function(err, result) {
+        if (err) {
+            alert(err);
+            return;
+        }
+        console.log('call result: ' + result);
+      });
+    }
+
+    signUserOut = function(cognitoUser) {
+      if (cognitoUser != null) {
+          cognitoUser.signOut();
+      }
+    }
+
+    signUserOutGlobally = function(cognitoUser) {
+      cognitoUser.globalSignOut();
+    }
+
+    getCurrentUserFromLocalStorage = function(userPool) {
+      var cognitoUser = userPool.getCurrentUser();
+
+      if (cognitoUser != null) {
+        cognitoUser.getSession(function(err, session) {
+            if (err) {
+               alert(err);
+                return;
+            }
+            console.log('session validity: ' + session.isValid());
+
+            AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                IdentityPoolId : '...' // your identity pool id here
+                Logins : {
+                    // Change the key below according to the specific region your user pool is in.
+                    'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>' : session.getIdToken().getJwtToken()
+                }
+            });
+
+            // Instantiate aws sdk service objects now that the credentials have been updated.
+            // example: var s3 = new AWS.S3();
+
+        });
+      }
+    }
+
+    confirmRegisteredUnauthenticatedUser = function(userName, confirmationCode) {
+      
+      function getUserPool() {
+        var poolData = { UserPoolId : 'us-east-1_TcoKGbf7n', ClientId : '4pe2usejqcdmhi0a25jp4b5sh3'};
+        var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
+        return userPool;
+      }
+
+      function getCognitoUser(userName, userPool) {
+        var userData = { Username : userName, Pool : userPool };
+        var cognitoUser = new AWSCognito.CognitoIdentityServiceProvider.CognitoUser(userData);
+        return cognitoUser();
+      }
+
+      var cognitoUser = getCognitoUser(userName, getUserPool());
+      
+      cognitoUser.confirmRegistration(confirmationCode, true, function(err, result) {
+          if (err) {
+              alert(err);
+              return;
+          }
+          console.log('call result: ' + result);
+      });
+    }
 
     return {
      
