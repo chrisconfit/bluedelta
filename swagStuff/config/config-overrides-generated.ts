c3621229a57538c31c4ad1d1a07c3c22f37f2cfa
Jake Corn <jake.corn@hatboysoftware.com>
2017-06-02 05:23:54 -0500

    pulling out swag stuff to step through backend credentials on front end

---
+++ swagStuff/config/config-overrides-generated.ts
@@ -0,0 +1,12 @@
+// Auto-generated file, do not modify directly
+
+const configOverridesGenerated = {
+  "REGION": "us-east-1",
+  "PROFILE_IMAGES_S3_BUCKET": "blue-delta-api-development-stack-userdatabucket-1roa31kj25t6q",
+  "API_ENDPOINT": "https://ritgmsrczb.execute-api.us-east-1.amazonaws.com/development",
+  "USER_POOL_ID": "us-east-1_hbjWOqI1M",
+  "CLIENT_ID": "1slpl86p8rf8ea6mhq00tl1b2r",
+  "IDENTITY_POOL_ID": "us-east-1:60cb9dd2-a77d-4f65-94eb-bf32a0a7cacf"
+};
+
+export { configOverridesGenerated }
