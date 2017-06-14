2b33314a39afa2c78b0c6afb85ee872c451fc6d3
Matthew Alan Gray <mgray@hatboysoftware.com>
2017-06-14 16:13:11 -0500

    Updating SDK

---
+++ public/lib/blue-delta-sdk/API/Client/Address.js
@@ -0,0 +1,37 @@
+goog.provide('API.Client.Address');
+
+/**
+ * @record
+ */
+API.Client.Address = function() {}
+
+/**
+ * @type {!string}
+ * @export
+ */
+API.Client.Address.prototype.addressLine1;
+
+/**
+ * @type {!string}
+ * @export
+ */
+API.Client.Address.prototype.addressLine2;
+
+/**
+ * @type {!string}
+ * @export
+ */
+API.Client.Address.prototype.city;
+
+/**
+ * @type {!string}
+ * @export
+ */
+API.Client.Address.prototype.state;
+
+/**
+ * @type {!string}
+ * @export
+ */
+API.Client.Address.prototype.zip;
+
