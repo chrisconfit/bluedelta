8f36372028db275a68af74afd8c8606d1837c926
Matthew Alan Gray <mgray@hatboysoftware.com>
2017-07-01 14:37:43 -0500

    Adding SDK and initial impl of auth

--- ng2-admin/src/app/pages/login/login.component.ts
+++ ng2-admin/src/app/pages/login/login.component.ts
@@ -1,5 +1,12 @@
-import {Component} from '@angular/core';
-import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import {
+  IUserLogin,
+  UserLoginService,
+  CognitoUtil,
+  UserState,
+  UserRegistrationService
+} from "../../../services/account-management.service";
 
 @Component({
   selector: 'login',
@@ -9,25 +16,38 @@ import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/form
 export class Login {
 
   public form:FormGroup;
-  public email:AbstractControl;
-  public password:AbstractControl;
   public submitted:boolean = false;
+  public userData: IUserLogin;
 
-  constructor(fb:FormBuilder) {
+  constructor(
+    public fb:FormBuilder,
+    protected userLoginService:UserLoginService
+  ) {
     this.form = fb.group({
       'email': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
       'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
     });
 
-    this.email = this.form.controls['email'];
-    this.password = this.form.controls['password'];
+    this.userData.username = this.form.controls['email'].value;
+    this.userData.password = this.form.controls['password'].value;
   }
 
   public onSubmit(values:Object):void {
     this.submitted = true;
     if (this.form.valid) {
+      this.login()
       // your code goes here
       // console.log(values);
     }
+  }
+
+  login(): void {
+    this.userLoginService.signIn(this.userData)
+      .then(() => {
+        // Login was successful
+      })
+      .catch((err: Error): void => {
+        // Login was unsuccessful
+      })
   }
 }
