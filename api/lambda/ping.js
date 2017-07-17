bb706b8253fd01d263828b89c97d4e5c467cd1eb
John McDonald <john.mcdonald@hatboysoftware.com>
2017-07-17 05:33:29 -0500

    api changes

--- api/lambda/ping.js
+++ api/lambda/ping.js
@@ -3,12 +3,12 @@ let rfr = require('rfr');
 let data = rfr('data');
 let wrapper = rfr('wrapper');
 
-function PingOperation(event, context) {
+function List(event, context) {
   return new Promise((resolve, reject) => {
     resolve({ response : "PONG" });
   });
 }
 
 module.exports = {
-  PingOperation,
+  List,
 };
